d006e3b1482ba4ce72be4937d50aeb90bfc3b0cc
zhangsj <945892500@qq.com>
2020-11-14 12:23:23 +0800

    解决swagger被拦截的问题

---
+++ src/main/java/com/yechrom/cloud/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.yechrom.cloud.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * 解决swagger被拦截的问题
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
+
